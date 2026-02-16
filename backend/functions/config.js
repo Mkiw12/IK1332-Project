33655a575dd55d815dc69ce5fb367514a05740bc
Mkiw12 <atisljarec2@gmail.com>
2026-02-16 14:35:07 +0100

    Email alarm

--- backend/functions/config.js
+++
@@ -1,8 +0,0 @@
-var config = {};
-
-config.host = "smtp.gmail.com";
-config.password = "euxw ucfk gzru kjkc";
-config.fromMail = "jonas.falk8920@gmail.com";
-config.toMail = "jonas.falk8920@gmail.com";
-
-module.exports = config;
